2a6c46b7fbc721718db1795fd4faec4f84da6ae0
Hazrat Ali <iloveyii@yahoo.com>
2019-04-09 13:05:12 +0200

    Search feature added

---
+++ src/sagas/searchSagas.js
@@ -0,0 +1,15 @@
+import { call, put } from 'redux-saga/effects';
+import api from '../api/feed';
+import { searchReadSuccessAction, searchReadFailAction } from "../actions/SearchAction";
+
+export function* searchReadSaga(action) {
+    try {
+        const resp = yield call(api.feed.search, action.payload.q);
+
+            console.log('Inside interestingReadSaga', action, resp);
+            yield put(searchReadSuccessAction(resp));
+    } catch (err) {
+        console.log('error', err);
+        yield put(searchReadFailAction(err));
+    }
+}
