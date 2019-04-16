a1c4f932750e5f3dbdc9384ff66388cf640793eb
Hazrat Ali <iloveyii@yahoo.com>
2019-04-16 10:16:37 +0200

    Added build dir

---
+++ build/precache-manifest.681d5cecd120eff2fabdd6301065bb2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f70b0875f7e5f391e7",
+    "url": "/static/css/main.fc855133.chunk.css"
+  },
+  {
+    "revision": "45f70b0875f7e5f391e7",
+    "url": "/static/js/main.d94ec44f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "db0502446588d575b0f2",
+    "url": "/static/js/2.086de5bb.chunk.js"
+  },
+  {
+    "revision": "11c0f492d95c56a3e0d907d9b95b3a20",
+    "url": "/index.html"
+  }
+];
